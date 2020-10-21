a64593bcb2a38078e9ba06565564536de8f58acc
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-21 14:51:33 -0400

    inital commit

--- routes/Words/models/Word.js
+++ routes/Words/models/Word.js
@@ -1,12 +1,10 @@
 const moment = require('moment');
+const mongoose=require('mongoose')
 
 const WordSchema = new mongoose.Schema({
   word: { type: String, required: true, lowercase: true, unique: true },
   meaning: { type: String, required: true, lowercase: true, trim: true },
-  date: {
-    type: String,
-    default: () => moment().format('dddd, MMMM, Do YYYY, h:mm a')
-  },
+  date: {type: String,default: () => moment().format('dddd, MMMM, Do YYYY, h:mm a')},
   timestamp: { type: Date, default: Date.now }
 });
 
